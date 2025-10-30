90ee84fbd7977d33f728e0a1250873064b358a7a
Trungdev1611 <103551327+Trungdev1611@users.noreply.github.com>
2025-10-30 20:58:34 +0700

    update swagger

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -6,7 +6,7 @@ import {
 } from '@nestjs/common';
 import { LoginDTO } from './auth.dto';
 import { CreateUserDTO } from 'src/users/user.dto';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiBody, ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 
 @ApiTags('Auth')
 @Controller('auth')
@@ -14,6 +14,24 @@ export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('login')
+    @ApiOperation({
+    summary: 'Đăng nhập tài khoản', 
+    description:
+      'Endpoint này cho phép người dùng đăng nhập vào hệ thống bằng email và mật khẩu. ' +
+      'Nếu thông tin hợp lệ, API sẽ trả về access token để sử dụng cho các yêu cầu tiếp theo.',
+  })
+    @ApiResponse({
+    status: 200,
+    description: 'Đăng nhập thành công. Trả về access token.',
+    schema: {
+      example: {
+        access_token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9...',
+        name: "example_name",
+        email: "example_email"
+
+      },
+    },
+  })
   loginAuth(@Body() loginData: LoginDTO) {
     return this.authService.login(loginData);
   }
--- src/auth/auth.dto.ts
+++ src/auth/auth.dto.ts
@@ -1,4 +1,13 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
 export class LoginDTO {
+
+    @ApiProperty({ example: 'user@example.com', description: 'Email của người dùng' })
+    @IsEmail()
     email: string
+
+    @ApiProperty({ example: '123456', description: 'Mật khẩu của người dùng' })
+    @IsString()
+    @IsNotEmpty()
     password: string
 }
